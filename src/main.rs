30dc4681315cf6e1ac768cbec7cd2ca8c4d91d07
André Lins <andrelucas01@hotmail.com>
2020-03-17 19:07:03 -0300

    Retornando numero por extenso

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
 fn numero_por_extenso<T: Into<f64>>(_valor: T) -> String {
     let mut string = String::new();
-    let _casas = ["", "mil", "milhão", "bilhão", "trilhão", "quatrilhão"];
-    let _centenas = ["", "cem", "duzentos", "trezentos", "quatrocentos","quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos"];
-    let _dezenas = ["", "dez", "vinte", "trinta", "quarenta", "cinquenta","sessenta", "setenta", "oitenta", "noventa"];
-    let _dezenas_compostas = ["dez", "onze", "doze", "treze", "quatorze", "quinze","dezesseis", "dezesete", "dezoito", "dezenove"];
-    let _unidades = ["", "um", "dois", "três", "quatro", "cinco", "seis","sete", "oito", "nove"];
-    
     let _valor_string = _valor.into().to_string();
     let _separar_decimal: Vec<&str> = _valor_string.split(".").collect();
 
@@ -17,14 +11,47 @@ fn numero_por_extenso<T: Into<f64>>(_valor: T) -> String {
     }
 
     let quantidade_centena = ((_inteiro.len() as f32) / 3.0).round() as i32;
-    
-    for contador_centena in (1..quantidade_centena+1) {
-        println!("{}", contador_centena);
-        // println!("{}", &_casas[contador_centena..contador_centena+1]);
-        // println!("{}", &_inteiro[inicio..inicio+1]);
-    }
 
-    string.push_str(_separar_decimal[0]);
+    for contador_centena in 0..quantidade_centena {
+        let tripla: String = _inteiro
+            .chars()
+            .skip((contador_centena * 3) as usize)
+            .take(3)
+            .collect();
+        let unidades_centena: Vec<&str> = tripla.split("").collect();
+
+        let centena = CENTENAS[unidades_centena[1].parse::<usize>().unwrap()];
+        let dezena = DEZENAS[unidades_centena[2].parse::<usize>().unwrap()];
+        let dezena_composta = DEZENAS_COMPOSTAS[unidades_centena[3].parse::<usize>().unwrap()];
+        let unidade = UNIDADES[unidades_centena[3].parse::<usize>().unwrap()];
+        let casa = CASAS[(quantidade_centena - contador_centena - 1) as usize];
+
+        if centena != "" {
+            string.push_str(centena);
+        }
+
+        if dezena == "dez" {
+            string.push_str(" e ");
+            string.push_str(dezena_composta);
+        } else {
+            if dezena != "" {
+                string.push_str(" e ");
+                string.push_str(dezena);
+            }
+
+            if casa == "mil" && unidade == "um" {
+                string.push_str("");
+            } else {
+                if unidade != "" {
+                    string.push_str(" e ");
+                    string.push_str(unidade);
+                }
+            }
+        }
+
+        string.push_str(casa);
+        string.push_str(" ");
+    }
 
     string
 }
@@ -38,5 +65,48 @@ fn normalizar(valor: String) -> String {
 }
 
 fn main() {
-    println!("{}", numero_por_extenso(1234));
+    println!("{}", numero_por_extenso(1215));
 }
+
+const CASAS: [&str; 6] = ["", "mil", "milhão", "bilhão", "trilhão", "quatrilhão"];
+
+const CENTENAS: [&str; 10] = [
+    "",
+    "cem",
+    "duzentos",
+    "trezentos",
+    "quatrocentos",
+    "quinhentos",
+    "seiscentos",
+    "setecentos",
+    "oitocentos",
+    "novecentos",
+];
+
+const DEZENAS: [&str; 10] = [
+    "",
+    "dez",
+    "vinte",
+    "trinta",
+    "quarenta",
+    "cinquenta",
+    "sessenta",
+    "setenta",
+    "oitenta",
+    "noventa",
+];
+const DEZENAS_COMPOSTAS: [&str; 10] = [
+    "dez",
+    "onze",
+    "doze",
+    "treze",
+    "quatorze",
+    "quinze",
+    "dezesseis",
+    "dezesete",
+    "dezoito",
+    "dezenove",
+];
+const UNIDADES: [&str; 10] = [
+    "", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove",
+];
